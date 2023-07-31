bbc72ff75fc0c4d1020bb9dbc2cbaaa3e7b13058
Aditi Jain <jaditi930@gmail.com>
2023-07-31 23:04:37 +0530

    ui done

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
+const API_KEY="3392dd216f9e4ec2ab74b42540932d56";
+function setNews(news_articles){
+  let news_container=document.getElementById("news")
+  news_container.innerHTML=""
+    for(let i=0;i<news_articles.length;i++)
+    {  
+        let news=news_articles[i];
+        news.description=(news.description==null)?" ":news.description;
+        console.log(typeof news.description)
+        news_container.innerHTML+=`<div class="news_box">
+        <div class="news_image"><img src=${news.urlToImage} alt=""></div>
+        <div class="body">
+        <div class="title">${news.title}</div>
+        <a class="read_more" href=${news.url}>Read More ...</a>
+        <div class="date">${news.publishedAt.split('T')[0]}</div>
+        </div>
+        </div>`   
+    }
+}
 window.onload=getNews();
-
-function getNews(){
-    let news_container=document.getElementById("news")
-    // let response=await fetch("https://newsapi.org/v2/top-headlines?\
-    // country=india&category=business&apiKey=3392dd216f9e4ec2ab74b42540932d56")
-    // let data=await response.json()
-    fetch("https://newsapi.org/v2/top-headlines?\
-    country=in&language=en&apiKey=3392dd216f9e4ec2ab74b42540932d56").then((response) => response.json())
+function getNews(type=0,query=""){
+  category_types=['general','business','technology','health','science','sports','entertainment']
+  category=category_types[type]
+    if(query=="")
+    fetch(`https://newsapi.org/v2/top-headlines?\
+    country=in&category=${category}&language=en&apiKey=${API_KEY}`).then((response) => response.json())
     .then((data) =>  {
-    console.log(data)
-
+      console.log(data);
+      setNews(data.articles)
 
     if(data.status!='ok')
     {
       news_container.innerHTML+="Could fetch News. Please try again later."
       return;  
-    }
-    for(let i=0;i<data.articles.length;i++)
-    {  
-        let news=data.articles[i];
-        news.description=(news.description==null)?" ":news.description;
-        console.log(typeof news.description)
-        news_container.innerHTML+=`<div class="news_box">
-        <div class="news_image"><img src=${news.urlToImage}></div>
-        <div class="body">
-        <div class="title">${news.title}</div>
-        <div class="description">${news.description}</div>
-        <div class="remarks">
-        ${news.publishedAt}
-        </div>
-        </div>
-        </div>`   
     }
   })
 
