662f2d5a2faf2d77227c4a9c61f8d6372f5663ec
Aditi Jain <jaditi930@gmail.com>
2023-08-02 06:32:32 +0530

    fa icons and search by query

--- news-app/src/components/App.js
+++ news-app/src/components/App.js
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNewspaper } from "@fortawesome/free-regular-svg-icons";
+import { faBriefcase,faMicrochip,faBaseballBatBall,faFlask,faVirusCovid,faFaceLaughWink, faSearch } from "@fortawesome/free-solid-svg-icons";
 import '../style.css'
 import axios from 'axios'
 function App() {
@@ -6,10 +9,18 @@ function App() {
 
     async function getNews(type=0){
         const API_KEY="3392dd216f9e4ec2ab74b42540932d56"
+        let query=document.getElementsByTagName("input")[0].value;
+        if (query==""){
          let category_types=['general','business','technology','health','science','sports','entertainment']
         let category=category_types[type]
         let response=await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&language=en&apiKey=${API_KEY}`)
         setNews(response.data.articles)
+        }
+        else{
+          console.log(query)
+          let response=await axios.get(`https://newsapi.org/v2/top-headlines?q=${query}&apiKey=${API_KEY}`)
+          setNews(response.data.articles)
+        }
       };
       useEffect(() => {
         getNews();
@@ -28,15 +39,15 @@ function App() {
     <>
     <nav>
         <ul>
-            <li onClick={()=>{getNews(0)}}>News Point <i className="fa-regular fa-newspaper icons"></i></li>
-            <li onClick={()=>{getNews(1)}}>Business <i className="fa-solid fa-briefcase icons"></i></li>
-            <li onClick={()=>{getNews(2)}}>Technnology <i className="fa-solid fa-microchip icons"></i></li>
-            <li onClick={()=>{getNews(5)}}>Sports <i className="fa-solid fa-baseball-bat-ball icons"></i></li>
-            <li onClick={()=>{getNews(4)}}>Science <i className="fa-solid fa-flask icons"></i></li>
-            <li onClick={()=>{getNews(3)}}>Health <i className="fa-solid fa-virus-covid icons"></i></li>
-            <li onClick={()=>{getNews(6)}}>Entertainment <i className="fa-solid fa-face-laugh-wink icons"></i></li>
+            <li onClick={()=>{getNews(0)}}>News Point <FontAwesomeIcon icon={faNewspaper}/></li>
+            <li onClick={()=>{getNews(1)}}>Business <FontAwesomeIcon icon={faBriefcase} /></li>
+            <li onClick={()=>{getNews(2)}}>Technnology <FontAwesomeIcon icon={faMicrochip}/></li>
+            <li onClick={()=>{getNews(5)}}>Sports <FontAwesomeIcon icon={faBaseballBatBall}/></li>
+            <li onClick={()=>{getNews(4)}}>Science <FontAwesomeIcon icon={faFlask}/></li>
+            <li onClick={()=>{getNews(3)}}>Health <FontAwesomeIcon icon={faVirusCovid}/></li>
+            <li onClick={()=>{getNews(6)}}>Entertainment <FontAwesomeIcon icon={faFaceLaughWink}/></li>
             <li id="search"><input type="text" placeholder="Search News"/>
-                 <i id="query" className="fa-solid fa-magnifying-glass icons" onClick={getNews}></i>
+                 <FontAwesomeIcon id="query" icon={faSearch}  onClick={getNews}/>
             </li>
         </ul>
     </nav>
